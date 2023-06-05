e3e3e347ceb96815a0d4c1934b04bdea1baeba47
Beatriz Silva <108994174+beatrizon@users.noreply.github.com>
2023-06-05 11:41:08 -0300

    aula02

---
+++ AppNotifica/Controller/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  AppNotifica
+//
+//  Created by IFBIOTIC10 on 05/06/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginView:UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
---
+++ AppNotifica/Coodinator/Coodinator.swift
@@ -0,0 +1,16 @@
+//
+//  Coodinator.swift
+//  AppNotifica
+//
+//  Created by IFBIOTIC10 on 05/06/23.
+//
+
+import Foundation
+
+protocol Coodinator {
+    
+    
+    
+    func start()
+    
+}
